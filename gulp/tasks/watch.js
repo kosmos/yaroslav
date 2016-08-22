a83d2da466d17909c0033fe457458e3005415bd3
Kosmos <kosmos.ehp@gmail.com>
2016-08-22 20:43:17 +0300

    Initial commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,14 @@
+var gulp   = require('gulp');
+var config = require('../config');
+
+gulp.task('watch', 
+    ['copy:watch',
+    
+    'jade:watch',
+    'iconfont:watch',
+    'sprite:svg:watch',
+    'sprite:png:watch',
+    'svgo:watch',
+    'webpack:watch',
+    'sass:watch'
+]);
