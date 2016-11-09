86b32b5bc4e30a39546ba15fd9d6899e8ccff6d9
Sergey Tsybulnik <sergeytsybulnik@Sergeys-MacBook-Pro.local>
2016-11-10 01:13:34 +0200

    - FIx preview: move data to yml - Add menu block - Fix header block

---
+++ src/blocks/menu/menu.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sergeytsybulnik on 11/9/16.
+ */
+var $ = require('jquery');
+$(document).ready(function(){
+    $('.navigation-list').on('click', function(){
+        alert('click');
+    })
+});
