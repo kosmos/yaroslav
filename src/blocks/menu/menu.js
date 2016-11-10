db8e297b6c67e4b86608bc72f7b517d3d66748ae
Kosmos <kosmos.ehp@gmail.com>
2016-11-10 19:52:38 +0200

    Пример создания модулей js.

--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,9 +1,15 @@
 /**
  * Created by sergeytsybulnik on 11/9/16.
  */
-var $ = require('jquery');
-$(document).ready(function(){
+
+import $ from 'jquery';
+
+const menu = () => {
+
     $('.navigation-list').on('click', function(){
         alert('click');
-    })
-});
+    });
+
+}
+
+module.exports =  menu;
