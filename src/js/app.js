ffce6dd8728e0fd7df90b8ad9ccabee66b174e24
kosmos <kosmos.ehp@gmail.com>
2016-11-14 21:04:56 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 import menu from '../blocks/menu/menu';
-import slideParallaxScene from '../blocks/money/money';
+import manyParalax from '../blocks/money/money';
 
 $('document').ready(function() {
     menu();
+    manyParalax();
 });
