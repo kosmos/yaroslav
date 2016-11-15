440c9258c056bf00415cc849bfd6a056b62edc88
andriy.tsepukh@intellecteu.com <Bizkvit1523>
2016-11-15 10:13:32 +0200

    Add parallax

--- src/blocks/money/money.js
+++ src/blocks/money/money.js
@@ -8,10 +8,10 @@ let controller = new ScrollMagic.Controller();
 
 let slideParallaxScene = new ScrollMagic.Scene({
     triggerElement: ".money",
-    triggerHook: 1,
+    triggerHook: 0.7,
     duration: '200%'
 })
-    .setTween(TweenMax.from('.money__main', 1, {y: '-30%', ease:Power0.easeNone}))
+    .setTween(TweenMax.from('.money__main', 1, {y: '-15%', opacity: 0, ease:Power0.easeNone}))
     .addTo(controller);
 }
 
