07fb8432891109fe1e0f6260c77b22782711d013
andriy.tsepukh@intellecteu.com <Bizkvit1523>
2016-11-17 01:51:01 +0200

    Sarkofag anymation test + positioning

--- src/blocks/sarkofag-view/sarkofag-view.js
+++ src/blocks/sarkofag-view/sarkofag-view.js
@@ -6,13 +6,24 @@ const sarkofag_view = () => {
 
     let controller = new ScrollMagic.Controller();
 
+
+    let t1 = new TimelineLite();
+
+    t1
+        .to('.sarkofag-view__line1', 1, {height: 50})
+        .to('.sarkofag-view__line1', 2, {width: 200})
+        .to('.sarkofag-view__text3', 3, {opacity: 100});
+
     let slideParallaxScene = new ScrollMagic.Scene({
         triggerElement: ".sarkofag-view",
-        triggerHook: 0.7,
-        duration: '70%'
+        triggerHook: 0.1
+        /*duration: '50%'*/
     })
-        .setTween(TweenMax.from('.sarkofag-view__line1', 1, {width: 0, height:0, ease:Power0.easeNone}))
+        .setTween(t1)
+        /*.setTween(TweenMax.from('.sarkofag-view__line1', 3, {width: 0}))*/
         .addTo(controller);
+
+
 }
 
 module.exports =  sarkofag_view;
